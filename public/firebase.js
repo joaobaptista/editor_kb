f12fb6ed7e5c273b4f86fee61d1ae41b9ce1075b
João Machado <joaob007@hotmail.com>
2021-02-02 19:54:33 -0300

    login_api_google

---
+++ public/firebase.js
@@ -0,0 +1,10 @@
+// initialize Firebase - copy settings from Firebase console
+var config = {
+  apiKey: 'AIzaSyC_jmfnYFsi6SewpdXZdGvmg5gmVc2US1U',
+  authDomain: 'arquitexture-7dd77.firebaseapp.com',
+  databaseURL: 'https://arquitexture-7dd77.firebaseio.com',
+  projectId: 'arquitexture-7dd77',
+  storageBucket: 'arquitexture-7dd77.appspot.com',
+  messagingSenderId: '211182616701',
+}
+firebase.initializeApp(config)
